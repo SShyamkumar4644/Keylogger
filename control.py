7aa6ca90f4e1eb51dd1fca9f9c10463117f3cb17
S Shyam kumar <50286888+SShyamkumar4644@users.noreply.github.com>
2021-06-27 11:16:48 +0530

    Add files via upload

---
+++ control.py
@@ -0,0 +1,13 @@
+from pynput.mouse import Controller
+from pynput.keyboard import Controller
+# (left to right, top to bottom)
+# from top-left of the screen you can imagine the top-left to be(0,0)
+def controlMouse():
+    mouse = Controller()
+    mouse.position = (100,200)
+
+def controlKeyboard():
+    keyboard = Controller()
+    keyboard.type("i am good!")
+
+controlKeyboard(i am good!)
